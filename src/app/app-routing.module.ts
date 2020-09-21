38e05096898a10aa4942d6a6bb01dd6e0e7f7b73
Luddoro <luddenr1@live.se>
2020-09-21 16:55:01 +0200

    created basic frontend with bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from "./homepage/homepage.component";
+import { UserpageComponent } from "./userpage/userpage.component";
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomepageComponent },
+  { path: 'user', component: UserpageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from "@angular/router";
+import { HomepageComponent } from './homepage/homepage.component';
+import { UserpageComponent } from './userpage/userpage.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomepageComponent,
+    UserpageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+
     BrowserAnimationsModule
   ],
   providers: [],
